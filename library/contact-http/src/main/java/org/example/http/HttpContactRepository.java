ff0dcd0d5c7d02ab8ff4857e3f7382397908757f
Nicholas Drone <ndrone@gmail.com>
2019-10-17 18:43:52 -0400

    Updating formatting

--- library/contact-http/src/main/java/org/example/http/HttpContactRepository.java
+++ library/contact-http/src/main/java/org/example/http/HttpContactRepository.java
@@ -13,164 +13,125 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-class HttpContactRepository implements ContactRepository
-{
+class HttpContactRepository implements ContactRepository {
 
-    private final OkHttpClient client;
-    private final ObjectMapper objectMapper;
-    private final String       uri;
+	private final OkHttpClient client;
+	private final ObjectMapper objectMapper;
+	private final String uri;
 
-    HttpContactRepository(OkHttpClient client, ObjectMapper objectMapper, String uri)
-    {
-        if (client == null)
-        {
-            throw new IllegalArgumentException("client must not be null");
-        }
-        this.client = client;
+	HttpContactRepository(OkHttpClient client, ObjectMapper objectMapper, String uri) {
+		if (client == null) {
+			throw new IllegalArgumentException("client must not be null");
+		}
+		this.client = client;
 
-        if (objectMapper == null)
-        {
-            throw new IllegalArgumentException("objectMapper must not be null");
-        }
-        this.objectMapper = objectMapper;
+		if (objectMapper == null) {
+			throw new IllegalArgumentException("objectMapper must not be null");
+		}
+		this.objectMapper = objectMapper;
 
-        if (uri == null || uri.isBlank())
-        {
-            throw new IllegalArgumentException("uri must not be null");
-        }
-        this.uri = uri;
-    }
+		if (uri == null || uri.isBlank()) {
+			throw new IllegalArgumentException("uri must not be null");
+		}
+		this.uri = uri;
+	}
 
-    @Override
-    public Contact findContact(Integer id) throws IllegalArgumentException, ContactException
-    {
-        ContactRepositoryValidator.notNull(id);
+	@Override
+	public Contact findContact(Integer id) throws IllegalArgumentException, ContactException {
+		ContactRepositoryValidator.notNull(id);
 
-        Request request = new Request.Builder().url(String.format("%s?id=%s", uri, id)).build();
+		Request request = new Request.Builder().url(String.format("%s?id=%s", uri, id)).build();
 
 		String responseBody = execute(request);
-		if (responseBody != null && !responseBody.isBlank())
-		{
-			try
-			{
+		if (responseBody != null && !responseBody.isBlank()) {
+			try {
 				return objectMapper.readValue(responseBody, Contact.class);
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				throw new ContactException(e.getMessage(), e);
 			}
-		}
-		else
-		{
+		} else {
 			return null;
 		}
-    }
+	}
 
-    private String execute(Request request)
-    {
-        String body = null;
-        try (Response response = client.newCall(request).execute())
-        {
-            if (!response.isSuccessful())
-            {
-                throw new IOException(
-                        String.format("Unexpected response code: %s", response.code()));
-            }
-            Optional<ResponseBody> responseBody = Optional.ofNullable(response.body());
-            if (responseBody.isPresent())
-            {
-                body = responseBody.get().string();
-            }
-        }
-        catch (IOException e)
-        {
-            throw new ContactException(e.getMessage(), e);
-        }
-        return body;
-    }
+	private String execute(Request request) {
+		String body = null;
+		try (Response response = client.newCall(request).execute()) {
+			if (!response.isSuccessful()) {
+				throw new IOException(
+						String.format("Unexpected response code: %s", response.code()));
+			}
+			Optional<ResponseBody> responseBody = Optional.ofNullable(response.body());
+			if (responseBody.isPresent()) {
+				body = responseBody.get().string();
+			}
+		} catch (IOException e) {
+			throw new ContactException(e.getMessage(), e);
+		}
+		return body;
+	}
 
-    @Override
-    public Iterable<Contact> findContacts(String firstName, String lastName)
-            throws IllegalArgumentException, ContactException
-    {
-        ContactRepositoryValidator.firstNameOrLastNameHasText(firstName, lastName);
+	@Override
+	public Iterable<Contact> findContacts(String firstName, String lastName)
+			throws IllegalArgumentException, ContactException {
+		ContactRepositoryValidator.firstNameOrLastNameHasText(firstName, lastName);
 
-        Request request = null;
-        if (firstName != null && lastName != null)
-        {
-            request = new Request.Builder()
-                    .url(String.format("%s?firstName=%s&lastName=%s", uri, firstName, lastName))
-                    .build();
-        }
-        else if (firstName != null)
-        {
-            request = new Request.Builder()
-                    .url(String.format("%s?firstName=%s", uri, firstName)).build();
-        }
-        else if (lastName != null)
-        {
-            request = new Request.Builder()
-                    .url(String.format("%s?lastName=%s", uri, lastName)).build();
-        }
-        String responseBody = execute(request);
-        if (responseBody != null && !responseBody.isBlank())
-        {
-            CollectionType collectionType = objectMapper.getTypeFactory()
-                    .constructCollectionType(List.class, Contact.class);
-            try
-            {
-                return objectMapper.readValue(responseBody, collectionType);
-            }
-            catch (IOException e)
-            {
+		Request request = null;
+		if (firstName != null && lastName != null) {
+			request = new Request.Builder()
+					.url(String.format("%s?firstName=%s&lastName=%s", uri, firstName, lastName))
+					.build();
+		} else if (firstName != null) {
+			request = new Request.Builder()
+					.url(String.format("%s?firstName=%s", uri, firstName)).build();
+		} else if (lastName != null) {
+			request = new Request.Builder()
+					.url(String.format("%s?lastName=%s", uri, lastName)).build();
+		}
+		String responseBody = execute(request);
+		if (responseBody != null && !responseBody.isBlank()) {
+			CollectionType collectionType = objectMapper.getTypeFactory()
+					.constructCollectionType(List.class, Contact.class);
+			try {
+				return objectMapper.readValue(responseBody, collectionType);
+			} catch (IOException e) {
 				throw new ContactException(e.getMessage(), e);
-            }
-        }
-        else
-        {
-            return new ArrayList<>();
-        }
-    }
+			}
+		} else {
+			return new ArrayList<>();
+		}
+	}
 
-    @Override
-    public Contact saveContact(Contact contact) throws
-            IllegalArgumentException, ContactException
-    {
-        ContactRepositoryValidator.notNull(contact);
-        Contact savedContact;
-        if (contact.getId() != null)
-        {
-            Contact existingContact = findContact(contact.getId());
-            if (existingContact == null)
-            {
-                throw new IllegalArgumentException(
-                        String.format("Contact with id: %s not found for updating",
-                                contact.getId()));
-            }
-            existingContact.setFirstName(contact.getFirstName());
-            existingContact.setLastName(contact.getLastName());
-        }
-        try
-        {
-            Request request = new Request.Builder().url(String.format("%s", uri))
-                    .post(RequestBody.create(
-                            objectMapper.writeValueAsString(contact),
-                            MediaType.parse("application/json")))
-                    .build();
-            String responseBody = execute(request);
-            if (responseBody != null && !responseBody.isBlank())
-            {
-                savedContact = objectMapper.readValue(responseBody, Contact.class);
-            }
-            else
-            {
-                savedContact = null;
-            }
-        }
-        catch (IOException e)
-        {
-            throw new ContactException(e.getMessage(), e);
-        }
-        return savedContact;
-    }
+	@Override
+	public Contact saveContact(Contact contact) throws
+			IllegalArgumentException, ContactException {
+		ContactRepositoryValidator.notNull(contact);
+		Contact savedContact;
+		if (contact.getId() != null) {
+			Contact existingContact = findContact(contact.getId());
+			if (existingContact == null) {
+				throw new IllegalArgumentException(
+						String.format("Contact with id: %s not found for updating",
+								contact.getId()));
+			}
+			existingContact.setFirstName(contact.getFirstName());
+			existingContact.setLastName(contact.getLastName());
+		}
+		try {
+			Request request = new Request.Builder().url(String.format("%s", uri))
+					.post(RequestBody.create(
+							objectMapper.writeValueAsString(contact),
+							MediaType.parse("application/json")))
+					.build();
+			String responseBody = execute(request);
+			if (responseBody != null && !responseBody.isBlank()) {
+				savedContact = objectMapper.readValue(responseBody, Contact.class);
+			} else {
+				savedContact = null;
+			}
+		} catch (IOException e) {
+			throw new ContactException(e.getMessage(), e);
+		}
+		return savedContact;
+	}
 }
--- library/contact-http/src/test/java/org/example/http/MockDispatcher.java
+++ library/contact-http/src/test/java/org/example/http/MockDispatcher.java
@@ -16,131 +16,94 @@ import java.util.Map;
 import java.util.Random;
 import java.util.stream.Collectors;
 
-class MockDispatcher extends Dispatcher
-{
+class MockDispatcher extends Dispatcher {
 
-    private ObjectMapper          objectMapper  = new ObjectMapper();
-    private Map<Integer, Contact> savedContacts = new HashMap<>();
+	private ObjectMapper objectMapper = new ObjectMapper();
+	private Map<Integer, Contact> savedContacts = new HashMap<>();
 
-    @NotNull
-    @Override
-    public MockResponse dispatch(RecordedRequest request) throws InterruptedException
-    {
+	@NotNull
+	@Override
+	public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
 
-        if ("POST".equals(request.getMethod()))
-        {
-            return saveContact(request);
-        }
-        else
-        {
-            HttpUrl requestUrl = request.getRequestUrl();
-            String id = requestUrl.queryParameter("id");
-            String firstName = requestUrl.queryParameter("firstName");
-            String lastName = requestUrl.queryParameter("lastName");
-            System.out.println(requestUrl + id);
-            if ("0".equals(id) || "1".equals(id))
-            {
-                return new MockResponse().setResponseCode(200);
-            }
-            else if (id != null)
-            {
-                Contact foundContact = savedContacts.get(Integer.valueOf(id));
-                if (foundContact != null)
-                {
-                    try
-                    {
-                        return new MockResponse().setResponseCode(200)
-                                .setBody(objectMapper.writeValueAsString(foundContact));
-                    }
-                    catch (JsonProcessingException e)
-                    {
-                        throw new InterruptedException(e.getMessage());
-                    }
-                }
-            }
-            else if ((firstName != null && !firstName.isBlank())
-                    && (lastName != null && !lastName.isBlank()))
-            {
+		if ("POST".equals(request.getMethod())) {
+			return saveContact(request);
+		} else {
+			HttpUrl requestUrl = request.getRequestUrl();
+			String id = requestUrl.queryParameter("id");
+			String firstName = requestUrl.queryParameter("firstName");
+			String lastName = requestUrl.queryParameter("lastName");
+			System.out.println(requestUrl + id);
+			if ("0".equals(id) || "1".equals(id)) {
+				return new MockResponse().setResponseCode(200);
+			} else if (id != null) {
+				Contact foundContact = savedContacts.get(Integer.valueOf(id));
+				if (foundContact != null) {
+					try {
+						return new MockResponse().setResponseCode(200)
+								.setBody(objectMapper.writeValueAsString(foundContact));
+					} catch (JsonProcessingException e) {
+						throw new InterruptedException(e.getMessage());
+					}
+				}
+			} else if ((firstName != null && !firstName.isBlank())
+					&& (lastName != null && !lastName.isBlank())) {
 				List<Contact> contacts = savedContacts.values().stream()
 						.filter(contact -> firstName.equals(contact.getFirstName()) &&
 								lastName.equals(contact.getLastName()))
 						.collect(Collectors.toList());
-				try
-				{
+				try {
 					return new MockResponse().setResponseCode(200)
 							.setBody(objectMapper.writeValueAsString(contacts));
-				}
-				catch (JsonProcessingException e)
-				{
+				} catch (JsonProcessingException e) {
 					throw new InterruptedException(e.getMessage());
 				}
-			}
-			else if (firstName != null && !firstName.isBlank())
-			{
+			} else if (firstName != null && !firstName.isBlank()) {
 				List<Contact> contacts = savedContacts.values().stream()
 						.filter(contact -> firstName.equals(contact.getFirstName()))
 						.collect(Collectors.toList());
-				try
-				{
+				try {
 					return new MockResponse().setResponseCode(200)
 							.setBody(objectMapper.writeValueAsString(contacts));
-				}
-				catch (JsonProcessingException e)
-				{
+				} catch (JsonProcessingException e) {
 					throw new InterruptedException(e.getMessage());
 				}
-			}
-			else if (lastName != null && !lastName.isBlank())
-			{
+			} else if (lastName != null && !lastName.isBlank()) {
 				List<Contact> contacts = savedContacts.values().stream()
 						.filter(contact -> lastName.equals(contact.getLastName()))
 						.collect(Collectors.toList());
-				try
-				{
+				try {
 					return new MockResponse().setResponseCode(200)
 							.setBody(objectMapper.writeValueAsString(contacts));
-				}
-				catch (JsonProcessingException e)
-				{
+				} catch (JsonProcessingException e) {
 					throw new InterruptedException(e.getMessage());
 				}
 			}
-        }
-        return new MockResponse().setResponseCode(404);
-    }
+		}
+		return new MockResponse().setResponseCode(404);
+	}
 
-    private MockResponse saveContact(RecordedRequest request) throws InterruptedException
-    {
-        String body = request.getBody().readUtf8();
-        try
-        {
-            Contact contact = objectMapper.readValue(body, Contact.class);
-            if (contact.getId() == null)
-            {
-                contact.setId(new Random().nextInt(Integer.MAX_VALUE));
-                savedContacts.put(contact.getId(), contact);
-                return new MockResponse().setResponseCode(200)
-                        .setBody(objectMapper.writeValueAsString(contact));
-            }
-            else
-            {
-                Contact foundContact = savedContacts.get(contact.getId());
-                if (foundContact != null)
-                {
-                    foundContact.setFirstName(contact.getFirstName());
-                    foundContact.setLastName(contact.getLastName());
-                    return new MockResponse().setResponseCode(200)
-                            .setBody(objectMapper.writeValueAsString(foundContact));
-                }
-                else
-                {
-                    return new MockResponse().setResponseCode(400);
-                }
-            }
-        }
-        catch (IOException e)
-        {
-            throw new InterruptedException(e.getMessage());
-        }
-    }
+	private MockResponse saveContact(RecordedRequest request) throws InterruptedException {
+		String body = request.getBody().readUtf8();
+		try {
+			Contact contact = objectMapper.readValue(body, Contact.class);
+			if (contact.getId() == null) {
+				contact.setId(new Random().nextInt(Integer.MAX_VALUE));
+				savedContacts.put(contact.getId(), contact);
+				return new MockResponse().setResponseCode(200)
+						.setBody(objectMapper.writeValueAsString(contact));
+			} else {
+				Contact foundContact = savedContacts.get(contact.getId());
+				if (foundContact != null) {
+					foundContact.setFirstName(contact.getFirstName());
+					foundContact.setLastName(contact.getLastName());
+					return new MockResponse().setResponseCode(200)
+							.setBody(objectMapper.writeValueAsString(foundContact));
+				} else {
+					return new MockResponse().setResponseCode(400);
+				}
+			}
+		} catch (IOException e) {
+			throw new InterruptedException(e.getMessage());
+		}
+	}
 }
